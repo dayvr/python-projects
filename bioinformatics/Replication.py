6732afb57b37265d6ca0249de55b4467b214eca3
Day Vazquez <dayvazquez@protonmail.com>
2023-09-21 15:37:12 -0400

    frequent_words function and some comments added

--- bioinformatics/Replication.py
+++ bioinformatics/Replication.py
@@ -1,3 +1,4 @@
+# Counts the number of times the specified pattern appears in the given text
 def pattern_count(text, pattern):
     count = 0
     for i in range(len(text)-len(pattern)+1):
@@ -6,12 +7,36 @@ def pattern_count(text, pattern):
     return count
 
 
+# Computes the frequency map of a given string Text and integer k
 def frequency_map(text, k):
-    frequency_map = {}
+    freq = {}
     n = len(text)
     for i in range(n-k+1):
         pattern = text[i:i+k]
-        frequency_map[pattern] = 0
-    return frequency_map
+        freq[pattern] = 0
+        for j in range(n-k+1):
+            if text[j:j+k] == pattern:
+                freq[pattern] += 1
+    return freq
 
+# Another version of the algorithm above that not uses a nested loop and finds the most frequent k-mers in a string Text
+def frequency_map2(text, k):
+    freq = {}
+    n = len(text)
+    for i in range(n-k+1):
+        pattern = text[i:i+k]
+        if pattern not in freq:
+            freq[pattern] = 1
+        else:
+            freq[pattern] += 1
+    return freq
 
+# Find the most frequent occurrence of words in a given text
+def frequent_words(text, k):
+    words = []
+    freq = frequency_map2(text, k)
+    m = max(freq.values())
+    for key in freq:
+        if freq[key] == m:
+            words.append(key)
+    return words
